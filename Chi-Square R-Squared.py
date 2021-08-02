22d3e0c8046cbdcfc9f7afb7a0846226d77feec4
Derick <78917746+dhanscom@users.noreply.github.com>
2021-08-02 19:59:03 -0400

    Chi-Square R-Squared
    Uses Fourier output to create a histogram of occurrences and then curve fits the discrete histogram to compare to the chi square distribution with two degrees of freedom and finds and R-Squared value

---
+++ Chi-Square R-Squared.py
@@ -0,0 +1,107 @@
+import os, os.path
+import numpy as np
+import csv
+import matplotlib.pyplot as plt
+from scipy.stats import chi2
+from scipy.stats import norm
+import scipy as sci
+import random as rand
+import numpy as np
+import math as math
+from scipy.stats import f, norm
+
+# make meta analysis program (figure out what needs to be calculated)
+
+numBins = 500
+
+
+def DistroFit(df, distroType, intervalStart, intervalEnd, n, bins, numBins):
+    x = np.linspace(intervalStart, intervalEnd, numBins)
+    nc = 1
+    try:
+        coefs, coVar = sci.optimize.curve_fit(lambda t, a, b: a * chi2.pdf(b * t, df), x, n, maxfev=1000)
+
+        fitPoints = coefs[0] * chi2.pdf(coefs[1] * x, df)
+        ssr = np.sum((n - fitPoints) ** 2)
+        sst = np.sum((n - np.average(fitPoints)) ** 2)
+
+        R2 = 1 - (ssr / sst)
+
+        if R2 < 0:
+            R2 = -1
+    except:
+        R2 = -1
+
+    return R2
+
+
+def makeMatrix(path, folder="."):
+    with open(folder + "/" + path, 'r') as f:
+        reader = csv.reader(f, delimiter=',')
+        matrix = np.array(list(reader)).astype(float)
+    return matrix
+
+
+input_folder = "Paul_Data"
+input_files = os.listdir(input_folder)
+
+results_folder = "Results"
+if not os.path.exists(results_folder):
+    os.mkdir(results_folder)
+else:
+    for file in os.listdir(results_folder):
+        os.remove(os.path.join(results_folder, file))
+
+file = open("Chi2FitTestOutput", "a")
+file.write("Event - Station - Band - R2 | Number of Bins: " + str(numBins) + " 90%  95%  99%" + "\n")
+file.close()
+
+countTotal = 0
+countPass90 = 0
+countPass95 = 0
+countPass99 = 0
+yn90 = ''
+yn95 = ''
+yn99 = ''
+
+freqCount = np.zeros(55)
+
+for input_file in input_files:
+    created_matrix = makeMatrix(input_file, input_folder)
+
+    # name_of_file = "Results_of_" + input_file.split("_")[1].split()[0] + "_" + input_file.split()[1] + "_" + input_file.split()[2].split(".")[0]
+    # name_of_entry = input_file.split("_")[1].split()[0] + "_" + input_file.split()[1] + "_" + input_file.split()[2].split(".")[0]
+    file = open("Chi2FitTestOutput", "a")
+    for count, row_index in enumerate(created_matrix):
+        countTotal += 1
+        n, bins, _ = plt.hist(row_index, bins=numBins, histtype='bar', log=False, color='b')
+
+        R2 = DistroFit(2, "chi2", bins[0], bins[numBins], n, bins, numBins)
+        yn90 = 'Fail'
+        yn95 = 'Fail'
+        yn99 = 'Fail'
+        if R2 >= .90:
+            countPass90 += 1
+            yn90 = 'Pass'
+        if R2 >= .95:
+            countPass95 += 1
+            yn95 = 'Pass'
+            freqCount[count] += 1
+        if R2 >= .99:
+            countPass99 += 1
+            yn99 = 'Pass'
+
+        file.write(
+            input_file.split()[1] + " " + input_file.split()[2].split(".")[0] + " " + str(count + 1) + " R2: " + str(
+                R2) + " " + yn90 + " " + yn95 + " " + yn99 + " " + "\n")
+        plt.clf()
+        print(count)
+    file.close()
+file = open("Chi2FitTestOutput", "a")
+file.write("Percent Pass: " + str(countPass90 / countTotal) + " " + str(countPass95 / countTotal) + " " + str(
+    countPass99 / countTotal) + "\n")
+file.write("Counts by Frequency Band 95% Threshold: \n")
+for count in range(0, 55):
+    file.write(str(count + 1) + " " + str(freqCount[count]) + "\n")
+file.close()
+print('here')
